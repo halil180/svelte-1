2057d7b53e6be42c6f151b85fb741dc2d90c55b1
halil180 <88166460+halil180@users.noreply.github.com>
2023-01-16 17:32:26 +0100

    Update App.svelte
    I think that the variable name "checked" is better than "yes" here

--- site/content/tutorial/06-bindings/03-checkbox-inputs/app-a/App.svelte
+++ site/content/tutorial/06-bindings/03-checkbox-inputs/app-a/App.svelte
@@ -1,18 +1,18 @@
 <script>
-	let yes = false;
+	let checked = false;
 </script>
 
 <label>
-	<input type=checkbox checked={yes}>
+	<input type=checkbox checked={checked}>
 	Yes! Send me regular email spam
 </label>
 
-{#if yes}
+{#if checked}
 	<p>Thank you. We will bombard your inbox and sell your personal details.</p>
 {:else}
 	<p>You must opt-in to continue. If you're not paying, you're the product.</p>
 {/if}
 
-<button disabled={!yes}>
+<button disabled={!checked}>
 	Subscribe
 </button>
